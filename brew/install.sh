7e1f8d91a45b6a8bbd090bd27b1cbaa2c2abbf8a
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2017-04-02 22:34:48 +1000

    Adding homebrew to dotfiles

---
+++ brew/install.sh
@@ -0,0 +1,13 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap homebrew/versions
+brew tap homebrew/dupes
+brew tap caskroom/cask
+brew tap homebrew/core
+brew tap homebrew/services
+brew tap neovim/neovim
+brew tap sebglazebrook/aliases
+brew tap sebglazebrook/infinite-pipe
+brew update
+brew upgrade
+
+brew install git imagemagick jq rust wget htop coreutils ripgrep watch gdb aliases infinite-pipe neovim/neovim/neovim
