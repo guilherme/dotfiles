5bcac394242449c876691cee395989aa1b0f7e12
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2017-04-02 22:15:30 +1000

    When vundle is already installed should just upgrade the repo

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,13 @@
 VIM_FILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -sfv "$VIM_FILES_DIR/.vimrc" ~
+
 mkdir -p ~/.vim/bundle/
-git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+if [ -d ~/.vim/bundle/Vundle.vim/.git ]; then
+  cd ~/.vim/bundle/Vundle.vim && git pull origin master
+else
+  git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
 vim +PluginInstall +qall
